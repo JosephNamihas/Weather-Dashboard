106fa0979e03b43b97470e3742b7b3819fb7bcfc
Joseph Namihas (Admin) <jnamihasadmin@bsuh.nhs.uk>
2023-01-30 15:14:01 +0000

    Returned data for current weather. Now working on 5 day forecast

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var apiKey = "835ac51cd6706255ca416c658e012750";
 
-
+// Gets the Coordinates
+//TODO Validation - What if user enters a city which doesn't exist?
 function getCoordinates(event) {
     event.preventDefault();
 
     var city = $("#search-input").val().trim();
-    
-    var GeoCodingQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q="+ city +"&limit=5&appid=835ac51cd6706255ca416c658e012750"; 
+    var geoURL = "http://api.openweathermap.org/geo/1.0/direct?q="+ city +"&limit=5&appid=835ac51cd6706255ca416c658e012750"; 
 
     $.ajax( {
-        url: GeoCodingQueryURL,
+        url: geoURL,
         method: "GET"
       }).then(function(response) {
           
@@ -25,15 +25,42 @@ function getCoordinates(event) {
 
 function getWeather(location) {
 
-    var url = "https://api.openweathermap.org/data/2.5/forecast?lat="+location.lat+"&lon="+location.lon+"&appid="+apiKey;
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+location.lat+"&lon="+location.lon+"&appid="+apiKey;
 
     $.ajax( {
-        url: url,
+        url: forecastURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
 
+        currentWeather(response);
+
     });
+
+
+    function currentWeather(response) {
+        let currentTemp = $("<p>").text("Current temperature: " + response.list[0].main.temp);
+        let currentHumidity = $("<p>").text("Current humidity: " + response.list[0].main.humidity);
+        let currentWindSpeed = $("<p>").text("Current wind speed: " + response.list[0].wind.speed);
+
+        $("#today").append(currentTemp);
+        $("#today").append(currentHumidity);
+        $("#today").append(currentWindSpeed);
+    }
+
+    function fiveDayForecast(response)
+ {
+    var day1 = { // an Array of objects? Or an object array?
+        time:
+        temp:
+        humidity:
+        windspeed:
+    }
+    }
+
+ }         // Function for current weather
+         // Function for 5 day forecast
+         // Validation - Valid city name?
 
 
 }
@@ -45,46 +72,5 @@ function getWeather(location) {
 
 
 
-
-
-
-   //TODO Add 2nd Location - How to add the 2nd geocoding URL?
-   // Add query URLs to an Array?
-
-
-   // -------------------  GETS FORECAST QUERY
-
-    /*var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+londonGeoLocation.lat+"&lon="+londonGeoLocation.lon+"&appid="+apiKey;
-
-    $.ajax( {
-        url: forecastQueryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-
-        var date = response.list[0].dt_txt;
-    
-        var temp = $("<p>").text("Temp: " + response.list[0].main.temp);
-        console.log(temp);
-        var windSpeed = $("<p>").text("Wind Speed: " + response.list[0].wind.speed);
-        var humidity = $("<p>").text("Humidity: " + response.list[0].main.humidity);
-
-
-        var dailyWeatherWindow = $("#today").text("Todays weather in <cityName>." +date);
-        dailyWeatherWindow.css({width: 50, height: 50, float: "right"})
-        console.log(temp);
-        $("#today").append(temp);
-        $("#today").append(windSpeed);
-        $("#today").append(humidity);
-        
-
-        var cityButton = $("<button>").text("Name of City");
-        $("#search-form").append(cityButton);
-
-    })
-  });
-  */
-  
-
-
+// Event listeners
   $("#search-form").on("submit", getCoordinates);
