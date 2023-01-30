9272bf2cbf4b1115a2644edb98defc25cbe9d696
Joseph Namihas (Admin) <jnamihasadmin@bsuh.nhs.uk>
2023-01-30 13:45:17 +0000

    Added URLS and recieved data

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
 var apiKey = "835ac51cd6706255ca416c658e012750";
 
-var londonGeoCodingQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=835ac51cd6706255ca416c658e012750";
 
-var barcelonaGeoCodingQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=Barcelona&limit=5&appid=835ac51cd6706255ca416c658e012750";
+function getCoordinates(event) {
+    event.preventDefault();
 
-// -------------------  GETS GEOLOCATION QUERY
-$.ajax( {
-    url: londonGeoCodingQueryURL,
-    method: "GET"
-  }).then(function(response) {
-  
+    var city = $("#search-input").val().trim();
+    
+    var GeoCodingQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q="+ city +"&limit=5&appid=835ac51cd6706255ca416c658e012750"; 
+
+    $.ajax( {
+        url: GeoCodingQueryURL,
+        method: "GET"
+      }).then(function(response) {
+          
     console.log(response);
 
-   var londonGeoLocation = {
-    lat: response[0].lat,
-    lon: response[0].lon,
-   }
+    var geoLocation = {
+     lat: response[0].lat,
+     lon: response[0].lon,
+    }
+    getWeather(geoLocation);
+});
+}
+
+function getWeather(location) {
+
+    var url = "https://api.openweathermap.org/data/2.5/forecast?lat="+location.lat+"&lon="+location.lon+"&appid="+apiKey;
+
+    $.ajax( {
+        url: url,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+
+    });
+
+
+}
+
+
+
+
+
+
+
+
+
+
 
    //TODO Add 2nd Location - How to add the 2nd geocoding URL?
+   // Add query URLs to an Array?
 
 
    // -------------------  GETS FORECAST QUERY
 
-
-    var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+londonGeoLocation.lat+"&lon="+londonGeoLocation.lon+"&appid="+apiKey;
+    /*var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+londonGeoLocation.lat+"&lon="+londonGeoLocation.lon+"&appid="+apiKey;
 
     $.ajax( {
         url: forecastQueryURL,
@@ -33,13 +64,27 @@ $.ajax( {
 
         var date = response.list[0].dt_txt;
     
-        var temp = response.list[0].main.temp;
-        var windSpeed = response.list[0].wind.speed;
-        var humidity = response.list[0].main.humidity;
-    
-        console.log("London: \n"+date+"\nTemp: "+temp+"\nWindspeed: "+windSpeed+"\nHumidity: "+humidity);
+        var temp = $("<p>").text("Temp: " + response.list[0].main.temp);
+        console.log(temp);
+        var windSpeed = $("<p>").text("Wind Speed: " + response.list[0].wind.speed);
+        var humidity = $("<p>").text("Humidity: " + response.list[0].main.humidity);
+
+
+        var dailyWeatherWindow = $("#today").text("Todays weather in <cityName>." +date);
+        dailyWeatherWindow.css({width: 50, height: 50, float: "right"})
+        console.log(temp);
+        $("#today").append(temp);
+        $("#today").append(windSpeed);
+        $("#today").append(humidity);
+        
+
+        var cityButton = $("<button>").text("Name of City");
+        $("#search-form").append(cityButton);
+
     })
   });
+  */
+  
 
 
-  // 
+  $("#search-form").on("submit", getCoordinates);
